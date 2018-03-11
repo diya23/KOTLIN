72468c890c2f052e0845f92534a9e8016e712013
diya23 <diyanabram@gmail.com>
2018-03-11 18:29:07 +0530

    practise kotlin

---
+++ systemdate.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String>)
+{
+    var year= Calendar.getInstance().get(Calendar.YEAR)
+    var month=Calendar.getInstance().get(Calendar.MONTH)
+    var date=Calendar.getInstance().get(Calendar.DATE)
+    println("DATE IS $date ")
+    println("MONTH IS $month")
+    println("YEAR IS $year")
+}
